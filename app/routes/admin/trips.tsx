66010d08e0d6bc4e2f0f57f1cdfbcc4f5734a746
Adil Imran <devadem1370@gmail.com>
2025-05-13 09:00:08 -0700

    complete create trip page

---
+++ app/routes/admin/trips.tsx
@@ -0,0 +1,18 @@
+import Header from 'components/Header';
+import React from 'react'
+
+const Trips = () => {
+  return (
+    <main className='all-users wrapper'>
+         <Header
+        title="Trips"
+        description="View and edit AI-generated  travel plans"
+        ctaText="Create a trip"
+        ctaUrl="/trips/create"
+
+      />
+    </main>
+  )
+}
+
+export default Trips;
